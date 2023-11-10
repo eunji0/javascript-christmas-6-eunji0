26c80dd182c74bfcc813a8100af5a9ddb068e1e4
unknown <pih9134@naver.com>
2023-11-11 00:53:13 +0900

    feat: 에러메시지 상수화

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+const ERROR_MESSAGE = Object.freeze({
+  errorText: '[ERROR]',
+  invalidDate: '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
+  invalidOrder: '유효하지 않은 주문입니다. 다시 입력해 주세요.',
+});
+
+const DOES_NOT_EXIST = Object.freeze('없음');
+
+export { ERROR_MESSAGE, DOES_NOT_EXIST };
