7442f7f5f776b32c30fc4c07cf6c484b85509171
unknown <pih9134@naver.com>
2023-11-13 21:47:07 +0900

    feat: InputView 작성

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,69 @@
-export default InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
+// import { Console } from '@woowacourse/mission-utils';
+// import { PRINT_MESSAGE } from './constants.js';
+// import InputValidator from './InputValidator.js';
+
+// const InputView = {
+//   async getInputValidate(userInput, validate) {
+//     const input = await userInput();
+//     validate(input);
+//     return input;
+//   },
+
+//   async visitDate() {
+//     const dateInput = await InputView.getInputValidate(
+//       async () => Console.readLineAsync(PRINT_MESSAGE.askVisitDate),
+//       InputValidator.visitDateInput,
+//     );
+//     return Number(dateInput);
+//   },
+
+//   async orderMenuCount() {
+//     const orderInput = await InputView.getInputValidate(
+//       async () => Console.readLineAsync(PRINT_MESSAGE.askMenuCount),
+//       InputValidator.menuCountInput,
+//     );
+//     return orderInput.split(',').map((item) => item.trim());
+//   },
+// };
+
+// export default InputView;
+import { Console } from '@woowacourse/mission-utils';
+import { PRINT_MESSAGE } from './constants.js';
+import InputValidator from './InputValidator.js';
+
+const InputView = {
+  async getInputValidate(userInput, validate) {
+    let input;
+    let isValidInput = false;
+
+    while (!isValidInput) {
+      try {
+        input = await userInput();
+        validate(input);
+        isValidInput = true;
+      } catch (error) {
+        Console.print(error.message);
+      }
     }
-    // ...
-}
+
+    return input;
+  },
+
+  async visitDate() {
+    const dateInput = await InputView.getInputValidate(
+      async () => Console.readLineAsync(PRINT_MESSAGE.askVisitDate),
+      InputValidator.visitDateInput,
+    );
+    return Number(dateInput);
+  },
+
+  async orderMenuCount() {
+    const orderInput = await InputView.getInputValidate(
+      async () => Console.readLineAsync(PRINT_MESSAGE.askMenuCount),
+      InputValidator.menuCountInput,
+    );
+    return orderInput.split(',').map((item) => item.trim());
+  },
+};
+
+export default InputView;
