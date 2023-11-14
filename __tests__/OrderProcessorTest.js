004663137780111b96f41c90230dd4a6b44bde5e
unknown <pih9134@naver.com>
2023-11-15 02:10:44 +0900

    chore: 주석 제거

--- __tests__/OrderProcessorTest.js
+++ __tests__/OrderProcessorTest.js
@@ -5,16 +5,13 @@ describe('OrderProcessor에 대한 Test', () => {
   const createOrderProcessor = (order) => new OrderProcessor(order);
 
   it('주문이 주어진 경우, 주문 내역과 할인 전 총 주문 금액이 계산되어야 한다.', () => {
-    // Given
     const order = ['티본스테이크-1', '바비큐립-2', '초코케이크-3'];
     const expectedOrderDetails = ['티본스테이크 1개', '바비큐립 2개', '초코케이크 3개'];
     const expectedBeforeDiscountTotalPrice =
       MENU_PRICES['티본스테이크'] * 1 + MENU_PRICES['바비큐립'] * 2 + MENU_PRICES['초코케이크'] * 3;
 
-    // When
     const orderProcessor = createOrderProcessor(order);
 
-    // Then
     expect(orderProcessor.orderAndPrice.orderDetails).toEqual(expectedOrderDetails);
     expect(orderProcessor.orderAndPrice.beforeDiscountTotalPrice).toEqual(
       expectedBeforeDiscountTotalPrice,
@@ -22,15 +19,12 @@ describe('OrderProcessor에 대한 Test', () => {
   });
 
   it('주문이 없는 경우, 주문 내역과 할인 전 총 주문 금액이 빈 값으로 초기화되어야 한다.', () => {
-    // Given
     const order = [];
     const expectedOrderDetails = [];
     const expectedBeforeDiscountTotalPrice = 0;
 
-    // When
     const orderProcessor = createOrderProcessor(order);
 
-    // Then
     expect(orderProcessor.orderAndPrice.orderDetails).toEqual(expectedOrderDetails);
     expect(orderProcessor.orderAndPrice.beforeDiscountTotalPrice).toEqual(
       expectedBeforeDiscountTotalPrice,
