5b9651189164be2203531b3e427ce77219461ada
unknown <pih9134@naver.com>
2023-11-14 21:33:49 +0900

    refactor: InputCheck 함수 분리

--- src/InputCheck.js
+++ src/InputCheck.js
@@ -12,7 +12,6 @@ const checkInputType = (input, errorMessage) => {
   }
 };
 
-// 각 주문 항목을 유효성 검사하는 함수
 const checkValidateOrderItem = (orderItem) => {
   const [menu, quantity] = orderItem.split('-').map((item) => item.trim());
   const isMenuValid = Object.values(MENU_LIST).flat().includes(menu);
@@ -24,13 +23,15 @@ const checkValidateOrderItem = (orderItem) => {
   return { menu, quantity: Number(quantity) };
 };
 
-// 주문 내 중복된 메뉴와 음료만 주문시 확인하는 함수
 const checkForDuplicateMenus = (orderDetails) => {
   const uniqueMenus = new Set(orderDetails.map((item) => item.menu));
+
   if (uniqueMenus.size !== orderDetails.length) {
     throw new Error(`${ERROR_MESSAGE.errorText}${ERROR_MESSAGE.invalidOrder}`);
   }
+};
 
+const checkForOnlyDrink = (orderDetails) => {
   const notDrink = new Set(
     orderDetails.filter((item) => !MENU_LIST.음료.includes(item.menu)).map((item) => item.menu),
   );
@@ -40,7 +41,6 @@ const checkForDuplicateMenus = (orderDetails) => {
   }
 };
 
-// 날짜가 유효한지 확인하는 함수
 const checkValidDate = (date) => {
   checkInputFormat(date, ERROR_MESSAGE.invalidDate);
   checkInputType(date, ERROR_MESSAGE.invalidDate);
@@ -51,14 +51,11 @@ const checkValidDate = (date) => {
   }
 };
 
-// 전체 주문의 유효성을 확인하는 함수
 const checkValidOrder = (order) => {
   checkInputFormat(order, ERROR_MESSAGE.invalidOrder);
-
   const orderDetails = order.split(',').map(checkValidateOrderItem);
-
   checkForDuplicateMenus(orderDetails);
-
+  checkForOnlyDrink(orderDetails);
   return orderDetails;
 };
 
