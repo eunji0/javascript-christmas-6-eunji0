64a982fd04b0c142fcb8658abb7fba3862f77f37
unknown <pih9134@naver.com>
2023-11-15 00:05:55 +0900

    refactor: validatorTest DRY원칙 적용

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -18,32 +18,32 @@ describe('InputValidator에 대한 Test', () => {
   describe('menuCountInput', () => {
     it('유효한 주문에 대해 에러를 던지지 않고 값을 반환한다.', () => {
       const validOrder = '티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1';
-      expect(() => InputValidator.menuCountInput(validOrder)).not.toThrow();
+      expect(() => testMenuCountInput(validOrder)).not.toThrow();
       expect(validOrder).toEqual('티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1');
     });
 
     it('메뉴에 없는 메뉴 주문 시 에러를 던져야 한다.', () => {
       const invalidOrder = '티본스테이크-1,바비큐립-1,초코케이크-2,사이다-1';
-      expect(() => InputValidator.menuCountInput(invalidOrder)).toThrow(ERROR_MESSAGE.invalidOrder);
+      expect(() => testMenuCountInput(invalidOrder)).toThrow(ERROR_MESSAGE.invalidOrder);
     });
 
     it('음료만 주문 시 에러를 던져야 한다.', () => {
       const invalidOrder = '제로콜라-2,레드와인-3';
-      expect(() => InputValidator.menuCountInput(invalidOrder)).toThrow(ERROR_MESSAGE.invalidOrder);
+      expect(() => testMenuCountInput(invalidOrder)).toThrow(ERROR_MESSAGE.invalidOrder);
     });
 
     it('중복된 메뉴에 대해 에러를 던져야 한다.', () => {
       const duplicateOrder = '티본스테이크-1,바비큐립-1,바비큐립-2';
-      expect(() => InputValidator.menuCountInput(duplicateOrder)).toThrow(
-        ERROR_MESSAGE.invalidOrder,
-      );
+      expect(() => testMenuCountInput(duplicateOrder)).toThrow(ERROR_MESSAGE.invalidOrder);
     });
 
     it('최대 총 주문 개수를 초과하는 경우 에러를 던져야 한다.', () => {
       const orderExceedingMaxCount = '티본스테이크-1,바비큐립-1,제로콜라-20';
-      expect(() => InputValidator.menuCountInput(orderExceedingMaxCount)).toThrow(
-        ERROR_MESSAGE.invalidOrder,
-      );
+      expect(() => testMenuCountInput(orderExceedingMaxCount)).toThrow(ERROR_MESSAGE.invalidOrder);
     });
   });
+
+  function testMenuCountInput(order) {
+    return InputValidator.menuCountInput(order);
+  }
 });
